319d7e5d6eafdaaddf0031b5c34675074338cda3
Miquel Angel Montero <mmontero2@institutjaumehuguet.cat>
2022-02-09 10:42:52 +0100

    commitFinal

--- src/main/mainApp.java
+++ src/main/mainApp.java
@@ -11,40 +11,48 @@ public class mainApp {
 		// TODO Auto-generated method stub
 		
 		//Antes de crear la base de datos la eliminamos si esta existe
-		boolean borrardb = Metodos_BBDD.borrarBBDD("tienda_i");
-		boolean creardb = Metodos_BBDD.crearBBDD("tienda_i");
+		boolean borrardb = Metodos_BBDD.borrarBBDD("dir");
+		boolean creardb = Metodos_BBDD.crearBBDD("dir");
 		
 		if(creardb) {		
 			System.out.println("La base de datos se ha creado exitosamente");		
 		}
 		
 		//Ejecutamos las sentencias de creacion de tablas
-		boolean creart1 = Metodos_BBDD.ejecutarSentencia("tienda_i",
-				 " CREATE TABLE fabricantes ("
-				+ "  codigo int NOT NULL,"
-				+ "  nombre varchar(100) NOT NULL,"
-				+ "  PRIMARY KEY (codigo)"
-				+ ");"
+		boolean creart1 = Metodos_BBDD.ejecutarSentencia("dir",
+				 "CREATE TABLE despachos ("
+				 + "	numero int not null,"
+				 + "    capacidad int,"
+				 + "    primary key (numero)"
+				 + ");"
 				);
 		
-		boolean creart2 = Metodos_BBDD.ejecutarSentencia("tienda_i", 
-				"CREATE TABLE articulos ("
-				+ "  codigo int NOT NULL,"
-				+ "  nombre varchar(100) NOT NULL,"
-				+ "  precio int NOT NULL,"
-				+ "  fabricante int NOT NULL,"
-				+ "  PRIMARY KEY (codigo),"
-				+ "  KEY fabricante_idx (fabricante),"
-				+ "  CONSTRAINT fabricante FOREIGN KEY (fabricante) REFERENCES fabricantes (codigo));");		
+		boolean creart2 = Metodos_BBDD.ejecutarSentencia("dir", 
+				"CREATE TABLE director ("
+				+ "	dni varchar(9) not null,"
+				+ "    nomApels nvarchar(255),"
+				+ "    dniJefe varchar(9) not null,"
+				+ "    despacho int not null,"
+				+ "    primary key (dni),"
+				+ "    constraint despacho"
+				+ "		foreign key (despacho)"
+				+ "        references despachos (numero)"
+				+ "        on update cascade"
+				+ "        on delete no action,"
+				+ "	constraint dniJ"
+				+ "		foreign key (dniJefe)"
+				+ "        references director (dni)"
+				+ "        on update cascade"
+				+ "        on delete no action);");		
 		
 		
 		//Insertamos datos
-		boolean insert1 = Metodos_BBDD.ejecutarSentencia("tienda_i", "INSERT INTO fabricantes VALUES (1, \"nombre2\");");
-		boolean insert2 = Metodos_BBDD.ejecutarSentencia("tienda_i", "INSERT INTO articulos VALUES (1, \"nombre1\", 30, 1);");
+		boolean insert1 = Metodos_BBDD.ejecutarSentencia("dir", "INSERT INTO despachos VALUES (1, 5000);");
+		boolean insert2 = Metodos_BBDD.ejecutarSentencia("dir", "INSERT INTO director VALUES (\"49874838M\", \"Miquel Angel Montero\", \"49874838M\", 1);");
 		
 		//Mostramos las tablas
-		Metodos_BBDD.mostrarValores("tienda_i", "fabricantes", 2);
-		Metodos_BBDD.mostrarValores("tienda_i", "articulos", 4);
+		Metodos_BBDD.mostrarValores("dir", "despachos", 2);
+		Metodos_BBDD.mostrarValores("dir", "director", 4);
 	
 	}
 
