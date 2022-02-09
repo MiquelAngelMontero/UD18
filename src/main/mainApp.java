2280f30ed2d2943c602c69b134584ff7fe5f8ff9
Miquel Angel Montero <mmontero2@institutjaumehuguet.cat>
2022-02-09 10:17:32 +0100

    commitFinal

--- src/main/mainApp.java
+++ src/main/mainApp.java
@@ -11,40 +11,42 @@ public class mainApp {
 		// TODO Auto-generated method stub
 		
 		//Antes de crear la base de datos la eliminamos si esta existe
-		boolean borrardb = Metodos_BBDD.borrarBBDD("tienda_i");
-		boolean creardb = Metodos_BBDD.crearBBDD("tienda_i");
+		boolean borrardb = Metodos_BBDD.borrarBBDD("alm");
+		boolean creardb = Metodos_BBDD.crearBBDD("alm");
 		
 		if(creardb) {		
 			System.out.println("La base de datos se ha creado exitosamente");		
 		}
 		
 		//Ejecutamos las sentencias de creacion de tablas
-		boolean creart1 = Metodos_BBDD.ejecutarSentencia("tienda_i",
-				 " CREATE TABLE fabricantes ("
-				+ "  codigo int NOT NULL,"
-				+ "  nombre varchar(100) NOT NULL,"
-				+ "  PRIMARY KEY (codigo)"
-				+ ");"
+		boolean creart1 = Metodos_BBDD.ejecutarSentencia("alm",
+				 "CREATE TABLE almacen ("
+				 + "  codigo int NOT NULL,"
+				 + "  lugar varchar(100) NOT NULL,"
+				 + "  capacidad int DEFAULT NULL,"
+				 + "  PRIMARY KEY (codigo)"
+				 + ");"
 				);
 		
-		boolean creart2 = Metodos_BBDD.ejecutarSentencia("tienda_i", 
-				"CREATE TABLE articulos ("
-				+ "  codigo int NOT NULL,"
-				+ "  nombre varchar(100) NOT NULL,"
-				+ "  precio int NOT NULL,"
-				+ "  fabricante int NOT NULL,"
-				+ "  PRIMARY KEY (codigo),"
-				+ "  KEY fabricante_idx (fabricante),"
-				+ "  CONSTRAINT fabricante FOREIGN KEY (fabricante) REFERENCES fabricantes (codigo));");		
+		boolean creart2 = Metodos_BBDD.ejecutarSentencia("alm", 
+				"CREATE TABLE cajas ("
+				+ "  numReferencia char(5) NOT NULL,"
+				+ "  contenido varchar(100) NOT NULL,"
+				+ "  valor int DEFAULT NULL,"
+				+ "  almacen int NOT NULL,"
+				+ "  PRIMARY KEY (numReferencia),"
+				+ "  KEY almacen_idx (almacen),"
+				+ "  CONSTRAINT almacen FOREIGN KEY (almacen) REFERENCES almacen (codigo)"
+				+ ");");		
 		
 		
 		//Insertamos datos
-		boolean insert1 = Metodos_BBDD.ejecutarSentencia("tienda_i", "INSERT INTO fabricantes VALUES (1, \"nombre2\");");
-		boolean insert2 = Metodos_BBDD.ejecutarSentencia("tienda_i", "INSERT INTO articulos VALUES (1, \"nombre1\", 30, 1);");
+		boolean insert1 = Metodos_BBDD.ejecutarSentencia("alm", "INSERT INTO almacen VALUES (1, \"Reus\", 30000);");
+		boolean insert2 = Metodos_BBDD.ejecutarSentencia("alm", "INSERT INTO cajas VALUES (\"ABCDE\", \"contenido\", 300, 1);");
 		
 		//Mostramos las tablas
-		Metodos_BBDD.mostrarValores("tienda_i", "fabricantes", 2);
-		Metodos_BBDD.mostrarValores("tienda_i", "articulos", 4);
+		Metodos_BBDD.mostrarValores("alm", "almacen", 3);
+		Metodos_BBDD.mostrarValores("alm", "cajas", 4);
 	
 	}
 
