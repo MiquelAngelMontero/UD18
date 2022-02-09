093aeda50d5cacbb2db6151c4b4e430ce7b4ac78
Miquel Angel Montero <mmontero2@institutjaumehuguet.cat>
2022-02-09 11:04:40 +0100

    commitFinal

--- src/main/mainApp.java
+++ src/main/mainApp.java
@@ -11,40 +11,56 @@ public class mainApp {
 		// TODO Auto-generated method stub
 		
 		//Antes de crear la base de datos la eliminamos si esta existe
-		boolean borrardb = Metodos_BBDD.borrarBBDD("tienda_i");
-		boolean creardb = Metodos_BBDD.crearBBDD("tienda_i");
+		boolean borrardb = Metodos_BBDD.borrarBBDD("cient");
+		boolean creardb = Metodos_BBDD.crearBBDD("cient");
 		
 		if(creardb) {		
 			System.out.println("La base de datos se ha creado exitosamente");		
 		}
 		
 		//Ejecutamos las sentencias de creacion de tablas
-		boolean creart1 = Metodos_BBDD.ejecutarSentencia("tienda_i",
-				 " CREATE TABLE fabricantes ("
-				+ "  codigo int NOT NULL,"
-				+ "  nombre varchar(100) NOT NULL,"
-				+ "  PRIMARY KEY (codigo)"
-				+ ");"
+		boolean creart1 = Metodos_BBDD.ejecutarSentencia("cient",
+				 "CREATE TABLE cientifico ("
+				 + "	dni varchar(9) not null,"
+				 + "    nomApels nvarchar(255),"
+				 + "    primary key (dni)"
+				 + ");"
 				);
 		
-		boolean creart2 = Metodos_BBDD.ejecutarSentencia("tienda_i", 
-				"CREATE TABLE articulos ("
-				+ "  codigo int NOT NULL,"
-				+ "  nombre varchar(100) NOT NULL,"
-				+ "  precio int NOT NULL,"
-				+ "  fabricante int NOT NULL,"
-				+ "  PRIMARY KEY (codigo),"
-				+ "  KEY fabricante_idx (fabricante),"
-				+ "  CONSTRAINT fabricante FOREIGN KEY (fabricante) REFERENCES fabricantes (codigo));");		
+		boolean creart2 = Metodos_BBDD.ejecutarSentencia("cient", 
+				"CREATE TABLE proyecto ("
+				+ "	id char(4) not null,"
+				+ "    nombre nvarchar(255),"
+				+ "    horas int,"
+				+ "    primary key (id)"
+				+ ");");		
 		
+		boolean creart3 = Metodos_BBDD.ejecutarSentencia("cient", 
+				"CREATE TABLE asignado_a ("
+				+ "	cientifico varchar(9) not null,"
+				+ "    proyecto char(4) not null,"
+				+ "    primary key (cientifico),"
+				+ "    constraint cient"
+				+ "		foreign key (cientifico)"
+				+ "        references cientifico (dni)"
+				+ "        on update cascade"
+				+ "        on delete no action,"
+				+ "	constraint proy"
+				+ "		foreign key (proyecto)"
+				+ "		references proyecto (id)"
+				+ "        on update cascade"
+				+ "        on delete no action"
+				+ ");");	
 		
 		//Insertamos datos
-		boolean insert1 = Metodos_BBDD.ejecutarSentencia("tienda_i", "INSERT INTO fabricantes VALUES (1, \"nombre2\");");
-		boolean insert2 = Metodos_BBDD.ejecutarSentencia("tienda_i", "INSERT INTO articulos VALUES (1, \"nombre1\", 30, 1);");
+		boolean insert1 = Metodos_BBDD.ejecutarSentencia("cient", "INSERT INTO cientifico VALUES (\"49874838M\", \"Miquel Angel Montero\");");
+		boolean insert2 = Metodos_BBDD.ejecutarSentencia("cient", "INSERT INTO proyecto VALUES (\"ABCD\", \"Pau Robuste\", 30);");
+		boolean insert3 = Metodos_BBDD.ejecutarSentencia("cient", "INSERT INTO asignado_a VALUES (\"49874838M\", \"ABCD\");");
 		
 		//Mostramos las tablas
-		Metodos_BBDD.mostrarValores("tienda_i", "fabricantes", 2);
-		Metodos_BBDD.mostrarValores("tienda_i", "articulos", 4);
+		Metodos_BBDD.mostrarValores("cient", "cientifico", 2);
+		Metodos_BBDD.mostrarValores("cient", "proyecto", 3);
+		Metodos_BBDD.mostrarValores("cient", "asignado_a", 2);
 	
 	}
 
